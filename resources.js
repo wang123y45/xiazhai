1a17d5a5ed83d3a13464706fb0fa544640bde72a
wang123y45 <50761779+wang123y45@users.noreply.github.com>
2025-09-19 16:46:45 +0800

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,243 @@
+// 资源数据 - 所有资源链接存储在这里
+window.resourceData = [
+    {
+        id: 1,
+        title: "变声器",
+        link: "https://pan.quark.cn/s/d2cb3d12449e",
+        extractCode: "",
+        password: "123456",
+        used: false
+    },
+    {
+        id: 2,
+        title: "A030-多款拉布布",
+        link: "https://pan.quark.cn/s/6fc10879e57c",
+        extractCode: "",
+        password: "234567",
+        used: false
+    },
+    {
+        id: 3,
+        title: "恒大歌舞团绝美视频",
+        link: "https://pan.quark.cn/s/c90da6da26be",
+        extractCode: "",
+        password: "345678",
+        used: false
+    },
+    {
+        id: 4,
+        title: "抖音蝴蝶号快手无人直播带货",
+        link: "https://pan.quark.cn/s/eb934525f20b",
+        extractCode: "",
+        password: "456789",
+        used: false
+    },
+    {
+        id: 5,
+        title: "黄金提炼配方法",
+        link: "https://pan.quark.cn/s/d536cb2e8c83",
+        extractCode: "",
+        password: "567890",
+        used: false
+    },
+    {
+        id: 6,
+        title: "比起XXXX我更喜欢你抖音B站鬼畜视频教程",
+        link: "https://pan.quark.cn/s/f6e5d73af0cf",
+        extractCode: "",
+        password: "678901",
+        used: false
+    },
+    {
+        id: 7,
+        title: "和平精英吃鸡教程",
+        link: "https://pan.quark.cn/s/043053276867",
+        extractCode: "",
+        password: "789012",
+        used: false
+    },
+    {
+        id: 8,
+        title: "编曲",
+        link: "https://pan.quark.cn/s/5dff819cb6cf",
+        extractCode: "",
+        password: "890123",
+        used: false
+    },
+    {
+        id: 9,
+        title: "司仪",
+        link: "https://pan.quark.cn/s/aa69182dbacd",
+        extractCode: "",
+        password: "901234",
+        used: false
+    },
+    {
+        id: 10,
+        title: "73张宝可梦宠物小精灵泳池4k高清壁纸",
+        link: "https://pan.quark.cn/s/1bbad52089db",
+        extractCode: "",
+        password: "012345",
+        used: false
+    },
+    {
+        id: 11,
+        title: "王者荣耀主页展示视频",
+        link: "https://pan.quark.cn/s/f17ed6f933b1",
+        extractCode: "",
+        password: "112233",
+        used: false
+    },
+    {
+        id: 12,
+        title: "印度火爆热门电影77部",
+        link: "https://pan.quark.cn/s/3b91fcd0083a",
+        extractCode: "",
+        password: "223344",
+        used: false
+    },
+    {
+        id: 13,
+        title: "数字人",
+        link: "https://pan.quark.cn/s/cff639ff128f",
+        extractCode: "",
+        password: "334455",
+        used: false
+    },
+    {
+        id: 14,
+        title: "炒菜教程",
+        link: "https://pan.quark.cn/s/e559460ec753",
+        extractCode: "",
+        password: "445566",
+        used: false
+    },
+    {
+        id: 15,
+        title: "PS视频教程",
+        link: "https://pan.quark.cn/s/195d12c3b06b",
+        extractCode: "",
+        password: "556677",
+        used: false
+    },
+    {
+        id: 16,
+        title: "唱歌教程",
+        link: "https://pan.quark.cn/s/30ccda038588",
+        extractCode: "",
+        password: "667788",
+        used: false
+    },
+    {
+        id: 17,
+        title: "台球教程",
+        link: "https://pan.quark.cn/s/09d78e50c47b",
+        extractCode: "",
+        password: "778899",
+        used: false
+    },
+    {
+        id: 18,
+        title: "化妆教程",
+        link: "https://pan.quark.cn/s/ec18ab5010fa",
+        extractCode: "",
+        password: "889900",
+        used: false
+    },
+    {
+        id: 19,
+        title: "剪映教程",
+        link: "https://pan.quark.cn/s/cfc91a5ee0a1",
+        extractCode: "",
+        password: "990011",
+        used: false
+    },
+    {
+        id: 20,
+        title: "手机摄影教程",
+        link: "https://pan.quark.cn/s/c5d74537bf97",
+        extractCode: "",
+        password: "001122",
+        used: false
+    },
+    {
+        id: 21,
+        title: "100部经典恐怖片",
+        link: "https://pan.quark.cn/s/ce382d4e3971",
+        extractCode: "",
+        password: "113355",
+        used: false
+    },
+    {
+        id: 22,
+        title: "中国西部刑侦大案纪实",
+        link: "https://pan.quark.cn/s/b4ed5c42f047",
+        extractCode: "",
+        password: "224466",
+        used: false
+    },
+    {
+        id: 23,
+        title: "豆瓣top250重口资料合集办公软件教程",
+        link: "https://pan.quark.cn/s/db1689b4cecd",
+        extractCode: "",
+        password: "335577",
+        used: false
+    },
+    {
+        id: 24,
+        title: "乒乓球教学",
+        link: "https://pan.quark.cn/s/edbc7426fffd",
+        extractCode: "",
+        password: "446688",
+        used: false
+    },
+    {
+        id: 25,
+        title: "办公教程",
+        link: "https://pan.quark.cn/s/fccc999dd938",
+        extractCode: "",
+        password: "557799",
+        used: false
+    },
+    {
+        id: 26,
+        title: "9000首经典音乐合集",
+        link: "https://pan.quark.cn/s/946f085ddc26",
+        extractCode: "",
+        password: "668800",
+        used: false
+    },
+    {
+        id: 27,
+        title: "去水印视频号下载",
+        link: "https://pan.quark.cn/s/f50bdfc13dbb#/list/share",
+        extractCode: "",
+        password: "779911",
+        used: false
+    },
+    {
+        id: 28,
+        title: "性格内向者高情商聊天视频教程",
+        link: "https://pan.quark.cn/s/49355cd2e9c5",
+        extractCode: "",
+        password: "880022",
+        used: false
+    },
+    {
+        id: 29,
+        title: "合集",
+        link: "https://pan.quark.cn/s/0246a72a6223",
+        extractCode: "",
+        password: "991133",
+        used: false
+    },
+    {
+        id: 30,
+        title: "水果音乐软件赠送包",
+        link: "https://pan.baidu.com/s/1SmDdNRrCvOMjUs7ZndNvbA",
+        extractCode: "6737",
+        password: "002244",
+        used: false
+    }
+];
